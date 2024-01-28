72e3ef054c37b3f8ced22f2ad86061cfe52ee53d
johntlehman <jtlehman@hey.com>
2024-01-27 17:30:18 -0800

    Started on CoopCode v1

---
+++ CoopCode_v1/CoopCode_v1.ino
@@ -0,0 +1,23 @@
+//Include statements
+
+//Declare variables
+lightBeforeSunriseMinutes //time lights will be on before sunrise
+noAlarmBefore //hhmm Lights will not turn on before this time
+
+void setup() {
+
+//Set first alarm for 6 a.m. the following morning
+
+}
+
+void loop() {
+//Put Board to sleep and wait for alarm interrupt
+
+//Alarm Interrupt - check that it's dark, check battery. If dark and battery >x%, turn lights on.
+
+//Sunrise Detected - set alarm for next day and put the board to sleep
+
+}
+
+// Function definitions
+
--- Display/Display.ino
+++ Display/Display.ino
@@ -1,13 +1,12 @@
 // Include the SparkFun qwiic OLED Library
 #include "SparkFun_Qwiic_OLED.h"
 
-QwiicNarrowO
-LED myOLED; //create an object called myOLED of the QwiicNarrowOLED class
+QwiicNarrowOLED myOLED; //create an object called myOLED of the QwiicNarrowOLED class
 
 void setup()
 {
     Serial.begin(115200);
-    Serial.println("Running OLED example");
+    Serial.println("Test Serial Comms");
 
     Wire.begin();
 
@@ -25,27 +24,32 @@ void setup()
     // Fill a rectangle on the screen that has a 4 pixel board
     myOLED.rectangleFill(4, 4, myOLED.getWidth() - 8, myOLED.getHeight() - 8);
 
-    String hello = "hello world!"; // our message
+    String printString = "tst% Batt"; // message to print to display
 
     // Center our message on the screen. Get the screen size of the "hello" string,
     // calling the getStringWidth() and getStringHeight() methods on the oled
 
     // starting x position - screen width minus string width  / 2
-    int x0 = (myOLED.getWidth() - myOLED.getStringWidth(hello)) / 2;
+    int x0 = (myOLED.getWidth() - myOLED.getStringWidth(printString)) / 2;
 
     // starting y position - screen height minus string height / 2 
-    int y0 = (myOLED.getHeight() - myOLED.getStringHeight(hello)) / 2;
+    int y0 = (myOLED.getHeight() - myOLED.getStringHeight(printString)) / 2;
 
     // Draw the text - color of black (0)
-    myOLED.text(x0, y0, hello, 0);
+    myOLED.text(x0, y0, printString, 0);
 
     // There's nothing on the screen yet - Now send the graphics to the device
     myOLED.display();
 
-    // That's it - HELLO!
-}
+}   
 
 void loop()
 {
-    delay(1000); // Do nothing
+    // Leave on for 2 seconds
+    myOLED.displayPower(true);
+    delay(2000);
+
+    // Turn off for 2 seconds
+    myOLED.displayPower(false);
+    delay(2000);
 }
