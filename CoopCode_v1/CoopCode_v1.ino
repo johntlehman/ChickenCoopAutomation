9ff371d30967d8515608b7a8457a75ddaf5c0eaa
johntlehman <jtlehman@hey.com>
2024-03-12 21:50:15 -0700

    RTC Interrupt alarm

--- CoopCode_v1/CoopCode_v1.ino
+++ CoopCode_v1/CoopCode_v1.ino
@@ -11,6 +11,13 @@ int dayCounter = 0;
 String currentDate;
 String currentTime;
 
+byte currentHour;
+byte currentMinute;
+byte currentSecond;
+byte alarmHour;
+byte alarmMinute;
+byte alarmSecond;
+
 const byte interruptPin = 2;
 
 void setup() {  
@@ -33,7 +40,6 @@ void setup() {
   rtc.set24Hour(); //set the RTC to use 24 hour time
 
 //---------------------Get Started with Actual Code--------------------------
-//Set first alarm for 6 a.m. the following morning
 }
 
 void loop() {
@@ -45,12 +51,34 @@ void loop() {
   }
   currentDate = rtc.stringDateUSA();
   currentTime = rtc.stringTime();
+  currentHour = rtc.getHours();
+  currentMinute = rtc.getMinutes();
+  currentSecond = rtc.getSeconds();
 
-  //Junk Code
-  delay(5000);
-  Serial.println("I just waited 5 more seconds");
+  alarmHour = currentHour;
+  alarmMinute = currentMinute;
+  alarmSecond = currentSecond + 15; //add 15 seconds
+
+  //Set the next day's alarm
+  rtc.setAlarm(alarmSecond, alarmMinute, alarmHour, 0, 0);
+  rtc.setAlarmMode(6); //6 = Alarm goes off every minute
+  rtc.enableInterrupt(INTERRUPT_AIE); //Enable the Alarm Interrupt
+
+  //DEBUG -  Print alarm date and time (Note that there is no year alarm register)
+  char alarmBuffer[20];
+  sprintf(alarmBuffer, "2020-%02d-%02dT%02d:%02d:%02d",
+          rtc.getAlarmMonth(),
+          rtc.getAlarmDate(),
+          rtc.getAlarmHours(),
+          rtc.getAlarmMinutes(),
+          rtc.getAlarmSeconds());
+  Serial.println(alarmBuffer);
 
 //Put Board to sleep and wait for alarm interrupt
+
+  //Junk Code
+  delay(60000);
+  Serial.println("I just waited 60 more seconds");
 
 //Alarm Interrupt - check that it's dark, check battery. If dark and battery >x%, turn lights on.
 //Sunrise Detected - set alarm for next day and put the board to sleep
