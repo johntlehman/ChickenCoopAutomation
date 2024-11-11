2c1d57fc2483e737cdd5928d9ee03c23bf6f33d2
johntlehman <jtlehman@hey.com>
2024-11-10 19:22:23 -0800

    Deleted Fuel Gauge junk from CoopCode

--- CoopCode_v1/CoopCode_v1.ino
+++ CoopCode_v1/CoopCode_v1.ino
@@ -1,9 +1,7 @@
 //Include statements
 #include <SparkFun_RV1805.h> //Library for RTC
-#include "SparkFun_MAX1704x_Fuel_Gauge_Arduino_Library.h"
 
 RV1805 rtc; //instantiate an object 'rtc' of the RV1805 class
-SFE_MAX1704X lipo(MAX1704X_MAX17043);  //Create an object called 'lipo' of the SFE_MAX1704X class
 
 //Declare variables
 unsigned long lightBeforeSunriseMinutes; //duration in minutes that lights will be on before sunrise
@@ -32,8 +30,6 @@ double soc = 0; // Variable to keep track of LiPo state-of-charge (SOC)
 bool alert; // Variable to keep track of whether alert has been triggered
 
 void setup() {  
-  pinMode(interruptPin, INPUT_PULLUP);
-  attachInterrupt(digitalPinToInterrupt(interruptPin), interruptPrint, LOW); //enable an interrupt pin to print a statement when grounded
   Wire.begin();
   Serial.begin(115200);
   Serial.println("Serial Comms Online");
@@ -51,22 +47,6 @@ void setup() {
   rtc.set24Hour(); //set the RTC to use 24 hour time
 
   lipo.enableDebugging(); // Uncomment this line to enable helpful debug messages on Serial
-
-  // Set up the MAX17043 LiPo fuel gauge:
-  if (lipo.begin() == false) // Connect to the MAX17043 using the default wire port
-  {
-    Serial.println(F("MAX17043 not detected. Please check wiring. Freezing."));
-    while (1)
-      ;
-  }
-
-	// Quick start restarts the MAX17043 in hopes of getting a more accurate
-	// guess for the SOC.
-	lipo.quickStart();
-
-	// We can set an interrupt to alert when the battery SoC gets too low.
-	// We can alert at anywhere between 1% - 32%:
-	lipo.setThreshold(20); // Set alert threshold to 20%.
 
 }
 
@@ -103,33 +83,7 @@ void loop() {
   rtc.setAlarmMode(6); //6 = Alarm goes off every minute
   rtc.enableInterrupt(INTERRUPT_AIE); //Enable the Alarm Interrupt
 
-  // lipo.getVoltage() returns a voltage value (e.g. 3.93)
-  voltage = lipo.getVoltage();
-  // lipo.getSOC() returns the estimated state of charge (e.g. 79%)
-  soc = lipo.getSOC();
-  // lipo.getAlert() returns a 0 or 1 (0=alert not triggered)
-  alert = lipo.getAlert();
-
   // Print the variables:
-  printStatus();
-
-  Serial.print("Voltage: ");
-  Serial.print(voltage);  // Print the battery voltage
-  Serial.println(" V");
-
-  Serial.print("Percentage: ");
-  Serial.print(soc); // Print the battery state of charge
-  Serial.println(" %");
-
-  Serial.print("Alert: ");
-  Serial.println(alert);
-  Serial.println();
-
-  //Put Board to sleep and wait for alarm interrupt
-
-  //Junk Code
-  delay(60000);
-  Serial.println("I just waited 60 more seconds");
   printStatus();
 
   //Alarm Interrupt - check that it's dark, check battery. If dark and battery >x%, turn lights on.
