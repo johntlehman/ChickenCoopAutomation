efdf74d002490b3828d012febff341b9e2a6110d
johntlehman <jtlehman@hey.com>
2024-12-03 20:48:33 -0800

    Quit using TimeLib.h because it sucks and I don't need it.

--- CoopCode_v1/CoopCode_v1.ino
+++ CoopCode_v1/CoopCode_v1.ino
@@ -1,7 +1,6 @@
 //Include statements
 #include <SparkFun_RV1805.h>; //Library for RTC
 #include <Qwiic_LED_Stick.h>; //Library for LED stick
-#include <TimeLib.h>;
 
 RV1805 rtc; //instantiate an object 'rtc' of the RV1805 class
 LED LEDStick; //Create an object of the LED class
@@ -12,28 +11,14 @@ unsigned long noAlarmBefore; //hhmm Lights will not turn on before this time
 int dayCounter = 0;
 
 //Use the time_t type to store timestamps in seconds. Allows use of TimeLib library functions
-time_t currentTime;
-time_t wakeyTime;
-time_t sunriseTime;
-time_t sunsetTime;
+unsigned long currentEpoch;
+unsigned long wakeyEpoch;
+unsigned long sunriseEpoch;
+unsigned long sunsetEpoch;
+
 //Code compares following two values and selects value which results in later WakeyTime
-const time_t delayFromSunset = 10*3600; //Minimum time without light used in fall/spring. default 10 hours (stored in seconds)
-const time_t delayFromSunrise = 22*3600; //Maximum Light-On time. used in midwinter. default of 22 hours (stored in seconds) results in lights on for 2 hours
-
-String currentDateString;
-String currentTimeString;
-
-byte currentMonth;
-byte currentDate;
-byte currentHour;
-byte currentMinute;
-byte currentSecond;
-
-byte alarmMonth;
-byte alarmDate;
-byte alarmHour;
-byte alarmMinute;
-byte alarmSecond;
+const unsigned long delayFromSunset = 10*3600; //Minimum time without light used in fall/spring. default 10 hours (stored in seconds)
+const unsigned long delayFromSunrise = 22*3600; //Maximum Light-On time. used in midwinter. default of 22 hours (stored in seconds) results in lights on for 2 hours
 
 void setup() {  
   Wire.begin();
@@ -60,17 +45,10 @@ void setup() {
   }
 }
 
-void loop() {
-  /*
-  currentTimeString = rtc.stringTime();
-  currentDateString = rtc.stringDateUSA();
-  
-  currentMonth = rtc.getMonth();
-  currentDate = rtc.getDate();
-  currentHour = rtc.getHours();
-  currentMinute = rtc.getMinutes();
-  currentSecond = rtc.getSeconds();
-  */
+void loop() {  
+  //call function to update RTC with current Epoch, then store it as wakeytime
+  GetCurrentEpoch();
+  wakeyEpoch = currentEpoch;
 
   //Store Wakeup Time
 
@@ -97,41 +75,15 @@ void loop() {
   //check that it's dark, check battery. If dark and battery >x%, turn lights on.
   //Sunrise Detected - set alarm for next day and put the board to sleep
 
-  //Test Code
-  //ReadLightSensor();
-
+  delay(1000); //delay for debug
 }
 
 // Function definitions
 void PrintStatus(){ //Prints status of all variables upon user request
-  if (rtc.updateTime() == false) //Updates the time variables from RTC
-  {
-    Serial.println("RTC failed to update");
-  }
-  
-  currentTime = rtc.getMonth();
-  delay(100);
-  Serial.println(currentTime + " is the Month");
-  delay(100);
-  //Serial.println(hour(currentTime) + " is the epoch hour");
-  //Serial.println(minute(currentTime) + " is the epoch minute");
-  wakeyTime = currentTime + 10*3600;
-  Serial.println(hour(wakeyTime) + " wakeyTime hours, this should be 10 hours later");   
-  //Serial.println(currentDateString + " " + currentTimeString); 
-  delay(10000);
 
-  /*
-  //DEBUG -  Print alarm date and time (Note that there is no year alarm register)
-  char alarmBuffer[20];
-  sprintf(alarmBuffer, "2024-%02d-%02dT%02d:%02d:%02d",
-          rtc.getAlarmMonth(),
-          rtc.getAlarmDate(),
-          rtc.getAlarmHours(),
-          rtc.getAlarmMinutes(),
-          rtc.getAlarmSeconds());
-  //Serial.println("Alarm is set for: ");
-  //Serial.println(alarmBuffer);
-  */
+  ReadLightSensor();
+  GetCurrentEpoch();
+
 }
 
 void ReadLightSensor(){
@@ -162,9 +114,20 @@ void ReadLightSensor(){
 
 }
 // Need to update battery voltage calc 
-void readBatteryVoltage(){
+void ReadBatteryVoltage(){
     const char analogPinBattery = A3; //Read battery voltage at analog pin A3
     
     int batteryValue = 0; //variable to store battery voltage raw value
     float batteryVoltageCalc;
+}
+
+void GetCurrentEpoch(){
+    if (rtc.updateTime() == false) //Updates the time variables from RTC
+  {
+    Serial.println("RTC failed to update");
+  }
+  
+  currentEpoch = rtc.getEpoch();
+  Serial.print("GetCurrentEpoch ran successfully. Epoch: ");
+  Serial.println(currentEpoch);
 }
