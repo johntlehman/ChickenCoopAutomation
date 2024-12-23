6dd56ddf68c3f2f2e63ea755e29ba95e51a69b1a
johntlehman <jtlehman@hey.com>
2024-12-23 09:39:16 -0500

    Method Updates, Adding Structure
    Updated methods to return a value. Added content to code to continue progress on coopcode v1.

--- CoopCode_v1/CoopCode_v1.ino
+++ CoopCode_v1/CoopCode_v1.ino
@@ -10,31 +10,46 @@ unsigned long lightBeforeSunriseMinutes; //duration in minutes that lights will 
 unsigned long noAlarmBefore; //hhmm Lights will not turn on before this time
 int dayCounter = 0;
 
-//Use the time_t type to store timestamps in seconds. Allows use of TimeLib library functions
 unsigned long currentEpoch;
 unsigned long wakeyEpoch;
 unsigned long sunriseEpoch;
 unsigned long sunsetEpoch;
 
+const char analogPinLightSensor = A0; // Read solar panel voltage at analog pin
+const char analogPinBattery = A3; //Read battery voltage at analog pin
+
 //Code compares following two values and selects value which results in later WakeyTime
 const unsigned long delayFromSunset = 10*3600; //Minimum time without light used in fall/spring. default 10 hours (stored in seconds)
 const unsigned long delayFromSunrise = 22*3600; //Maximum Light-On time. used in midwinter. default of 22 hours (stored in seconds) results in lights on for 2 hours
 
-void setup() {  
+float lightSensorVoltage; //used to track current light sensor voltage
+const float daylightVoltage = 3.3; //above this voltage is considered 'daylight'
+const float nightVoltage = 1.5; //below this voltage is considered 'night'
+
+float batteryVoltage; //used to track current battery voltage
+const float minBatteryVoltage = 11.8; //minimum battery voltage to enable lights
+
+void setup() 
+{  
   Wire.begin();
   Serial.begin(115200);
   Serial.println("Serial Comms Online");
 
-  if (rtc.begin() == false) {
+  if (rtc.begin() == false) 
+  {
     Serial.println("Something went wrong with RTC, check wiring");
   }  
-  else { 
-      Serial.println("RTC online!");
-    }
   
-  if (rtc.setToCompilerTime() == false) {
+  else 
+  { 
+    Serial.println("RTC online!");
+  }
+  
+  if (rtc.setToCompilerTime() == false) 
+  {
     Serial.println("Something went wrong setting the time");
   }
+  
   rtc.set24Hour(); //set the RTC to use 24 hour time
 
   //Use the time from the Arduino compiler (build time) to set the RTC
@@ -45,32 +60,75 @@ void setup() {
   }
 }
 
-void loop() {  
-  //call function to update RTC with current Epoch, then store it as wakeytime
-  GetCurrentEpoch();
-  wakeyEpoch = currentEpoch;
+void loop() 
+{  
+  //Waiting for sunrise, as code returns to loop start after alarm is set
+  
+  //alarm goes off
 
-  //Store Wakeup Time
+  //call function to update RTC with current Epoch and store it as wakeytime
+  wakeyEpoch = GetCurrentEpoch();
 
-  //Check light sensor reading
+  //Get battery voltage
+  batteryVoltage = GetBatteryVoltage();
+
+  //Get light sensor voltage
+  lightSensorVoltage = GetLightSensorVoltage();
+
+  PrintStatus(); //Print status of all variables
+  Seril.println("Wakey wakey sleepy chickens!");
 
   //If it's dark and this is not the first loop, turn the lights on
+  if (dayCounter > 0 & lightSensorVoltage < daylightVoltage & batteryVoltage > minBatteryVoltage)
+  {
+    //turn on lights
+    Serial.println("Lights on!");
+  }
+  
+  //Monitor for sunrise and battery level
+  while (lightSensorVoltage < daylightVoltage & batteryVoltage > minBatteryVoltage) //& time is less than sunrise time?
+  {
+      //sleep 8 seconds ish
+      
 
-  //Leave lights on while monitoring for sunrise and battery level
+      //update light sensor voltage and battery voltage
+      lightSensorVoltage = GetLightSensorVoltage();
+      batteryVoltage = GetBatteryVoltage();
+      Serial.println("Waiting for sunrise");
+      PrintStatus(); //print for debugging
+  }
 
   //Sunrise occurs - store sunrise time, next wakeup time, and turn off the lights
-
+  sunriseEpoch = GetCurrentEpoch();
+  
   //Monitor for sunset
-
+  while (lightSensorVoltage > nightVoltage) //& time is less than sunset time?
+  {
+    //sleep 8 seconds ish
+    lightSensorVoltage = GetLightSensorVoltage();
+  }
+  
   //Sunset occurs, store next wakeup time
+  sunsetEpoch = GetCurrentEpoch();
+  unsigned long sunriseWakey = sunriseEpoch + delayFromSunrise;
+  unsigned long sunsetWakey = sunsetEpoch + delayFromSunset;
+
+  if (sunriseWakey > sunsetWakey)
+  {
+    wakeyEpoch = sunriseWakey;
+  }
+
+  else
+  {
+    wakeyEpoch = sunsetWakey;
+  }
 
   //Wait for wakeup time
-
-
-  // Print the variables:
-  PrintStatus();
-
-  //first 
+  while (currentEpoch < wakeyEpoch)
+  {
+    //sleep 8 seconds ish
+    currentEpoch = GetCurrentEpoch();
+  }
   
   //check that it's dark, check battery. If dark and battery >x%, turn lights on.
   //Sunrise Detected - set alarm for next day and put the board to sleep
@@ -78,19 +136,25 @@ void loop() {
   delay(1000); //delay for debug
 }
 
-// Function definitions
-void PrintStatus(){ //Prints status of all variables upon user request
+// Function definitions----------------------------------------------
+void PrintStatus()
+{ //Prints status of all variables upon user request
+  float statusLightSensorVoltage = GetLightSensorVoltage();
+  float statusBatteryVoltage = GetBatteryVoltage();
+  unsigned long statusEpoch = GetCurrentEpoch();
 
-  ReadLightSensor();
-  GetCurrentEpoch();
-
+  Serial.print("Current Light Sensor Voltage: " + String(lightSensorVoltage,2));
+  Serial.print("Current Battery Voltage: " + String(batteryVoltage,2));
+  Serial.print("Current Epoch: " + String(currentEpoch));
+  
+  //Serial.printf("Current Light Sensor Voltage: %.2f", lightSensorVoltage);
+  // Serial.println("Current Light Sensor Voltage: " + lightSensorVoltage);
+  // Serial.println("Current Battery Voltage: " + batteryVoltage);
+  // Serial.println("Current Epoch: " + currentEpoch);
 }
 
-void ReadLightSensor(){
-    
-    const char analogPinLightSensor = A3; // Read solar panel voltage at analog pin A0
-    
-    unsigned long LoopStartTime = millis();
+float GetLightSensorVoltage()
+{
     float lightSensorValue = 0;
     float lightSensorCalc;
     float lightSensorSum = 0;
@@ -101,33 +165,29 @@ void ReadLightSensor(){
       lightSensorCalc = lightSensorValue * (3.3 / 1023.0);
       lightSensorSum = lightSensorSum + lightSensorCalc;
       averageLightSensorVoltage = lightSensorSum/i;
-      Serial.println(averageLightSensorVoltage);
+      Serial.println(averageLightSensorVoltage); //remove after debug
       delay(500);
     }
-    
-    unsigned long LoopTime = millis()-LoopStartTime;
 
-    Serial.print("Average Light Sensor Voltage Last ");
-    Serial.print(LoopTime/1000);
-    Serial.print(" seconds: ");
-    Serial.println(averageLightSensorVoltage);
+    return averageLightSensorVoltage;
 
 }
 // Need to update battery voltage calc 
-void ReadBatteryVoltage(){
-    const char analogPinBattery = A3; //Read battery voltage at analog pin A3
-    
-    int batteryValue = 0; //variable to store battery voltage raw value
-    float batteryVoltageCalc;
+float GetBatteryVoltage()
+{  
+  int batteryValue = 0; //variable to store battery voltage raw value
+  float batteryVoltageCalc;
+
+  return batteryVoltageCalc;
 }
 
-void GetCurrentEpoch(){
+unsigned long GetCurrentEpoch()
+{
     if (rtc.updateTime() == false) //Updates the time variables from RTC
-  {
+    {
     Serial.println("RTC failed to update");
-  }
+    }
   
   currentEpoch = rtc.getEpoch();
-  Serial.print("GetCurrentEpoch ran successfully. Epoch: ");
-  Serial.println(currentEpoch);
+  return currentEpoch;
 }
